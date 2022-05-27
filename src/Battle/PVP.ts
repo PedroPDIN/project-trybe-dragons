7d1b047ca66a40bce6296781385fc38c2876f0e0
Pedro Lima <pedro.oficial1969@gmail.com>
2022-05-27 13:56:08 -0300

    feat(req-10): adiciona requisito completo.

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -1,5 +1,21 @@
 import Battle from './Battle';
+import Fighter from '../Fighter';
 
 export default class PVP extends Battle {
-  
+  constructor(player1: Fighter, private _player2: Fighter) {
+    super(player1);
+  }
+
+  fight(): number {
+    this.player.attack(this._player2);
+    this._player2.attack(this.player);
+
+    this.player.special(this._player2);
+    this._player2.special(this.player);
+
+    this.player.attack(this._player2);
+    this._player2.attack(this.player);
+    
+    return super.fight();
+  }
 }
